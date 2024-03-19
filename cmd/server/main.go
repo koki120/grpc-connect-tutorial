d2aaae4e4a44fb93ede0e862aa8b6efd1b805990
nakatsu <k-nakatsu@digeon.co>
2024-03-19 17:23:05 +0900

    add cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -35,7 +35,16 @@ func (h *handler) Greet(ctx context.Context, req *connect.Request[greetv1.GreetR
 func main() {
 	mux := chi.NewRouter()
 	mux.Group(func(r chi.Router) {
-		r.Use(cors.Handler(cors.Options{}))
+		r.Use(cors.Handler(cors.Options{
+			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
 		path, handler := greetv1connect.NewGreetServiceHandler(NewHandler("Hello World"))
 		r.Handle(path+"*", handler)
 	})
